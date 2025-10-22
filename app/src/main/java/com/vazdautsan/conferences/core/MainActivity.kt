0a82f49c3d3342eb97296271824903422e308fd7
aleksey <a.gilev2807@mail.ru>
2025-10-22 15:04:11 +0300

    fix: status bar style

--- app/src/main/java/com/vazdautsan/conferences/core/MainActivity.kt
+++ app/src/main/java/com/vazdautsan/conferences/core/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.vazdautsan.conferences.core
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import com.vazdautsan.conferences.ui.theme.Colors
 import com.vazdautsan.conferences.ui.theme.ConferencesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT,
+                detectDarkMode = { false }
+            )
+        )
         setContent {
             ConferencesTheme {
                 NavGraph(modifier = Modifier.fillMaxSize())
